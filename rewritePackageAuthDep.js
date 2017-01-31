4b5ee9dd07db7854a65a69853f13f7927a1cf563
Cristian Cavalli <cristiancavall@google.com>
2017-01-31 10:52:30 -0800

    Initial commit

---
+++ rewritePackageAuthDep.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+var colors = require('colors');
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+var fs = require('fs');
+var stream = require('stream');
+var path = require('path');
+var Readable = require('stream').Readable
+var es = require('event-stream');
+var merge = require('lodash.merge');
+var noop = require('lodash.noop');
+var isFunction = require('lodash.isfunction');
+
+function rewritePackage (pathToPackageFile, newDependencyLocation, cb) {
+  var oldData = {};
+  var newData = {};
+  var pth = path.resolve(pathToPackageFile);
+  return fs.createReadStream(pth).pipe(es.wait())
+    .pipe(es.mapSync(function (buf) {
+      try {
+        merge(oldData, JSON.parse(buf.toString()));
+        merge(newData, oldData, {
+          dependencies: {
+            "google-auth-library": newDependencyLocation
+          }
+        });
+        console.log(colors.info('Package parsed, new package created..'));
+        return Buffer.from(JSON.stringify(newData));
+      } catch (e) {
+        console.log('got e', e);
+        done(e, null);
+        return;
+      }
+    }))
+    .pipe(es.mapSync(function (buf) {
+      console.log(colors.info('Original package file truncated..'));
+      fs.truncateSync(pth, 0);
+      return buf;
+    }))
+    .pipe(es.mapSync(function (buf) {
+      console.log(
+        colors.info('Writing new package contents..'));
+      return fs.createWriteStream(pth);
+    }))
+    .pipe(es.mapSync(function (writeStream) {
+      var s = new stream.Readable();
+      s._read = noop;
+      s.push(JSON.stringify(newData));
+      s.push(null);
+      s.pipe(writeStream);
+      
+      return writeStream;
+    })).pipe(es.mapSync(function (writeStream) {
+      writeStream.on('finish', function () {
+        console.log(colors.green('Write finished successfully!!'));
+        if(isFunction(cb)) {
+          cb({
+            oldData: oldData,
+            newData: newData,
+            path: pth
+          });
+        }
+      });
+      return writeStream;
+    })).on('error', function (e) {
+      console.log(colors.red.underline(
+        'Encountered an error trying to overwrite the package'));
+      console.log(colors.debug(e));
+    });
+};
+
+module.exports = rewritePackage;
---
+++ tests/unit/rewritePackageAuthDep.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var assert = require('assert');
+var stream = require('stream');
+var fs = require('fs');
+var path = require('path');
+var merge = require('lodash.merge');
+var noop = require('lodash.noop');
+var rewritePackageAuthDep = require('../../rewritePackageAuthDep.js');
+
+describe('Overwriting the google-auth-library depedency', function () {
+  var pth = path.resolve('./tests/fixtures/standard-package.json');
+  var originalContents;
+  before(function () {
+    originalContents = fs.readFileSync(pth);
+    originalContents = originalContents.toString();
+    originalContents = JSON.parse(originalContents);
+  });
+  after(function () {
+    fs.truncateSync(pth, 0);
+    var s = merge(new stream.Readable(), {_read: noop});
+    s.push(JSON.stringify(originalContents, null, 2));
+    s.push(null);
+    s.pipe(fs.createWriteStream(pth));
+  });
+  it('Should overwrite the fixture', function (done) {
+    rewritePackageAuthDep(pth, 
+      'git://github.com/cristiancavalli/google-auth-library-nodejs.git#replace-with-raw-http',
+      function (output) {
+        console.log('DONE!');
+        console.log(output.path);
+        console.log('--');
+        console.log(output.oldData);
+        console.log('--');
+        console.log(output.newData);
+        done();
+      }
+    );
+  });
+});
